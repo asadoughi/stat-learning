67a9533478a109d4dc945ce9e7b6ca9c0c027791
fred sallah <fsallah@users.noreply.github.com>
2016-09-12 22:19:10 -0400

    Update applied.R

--- ch2/applied.R
+++ ch2/applied.R
@@ -43,7 +43,7 @@ dim(Auto)
 summary(Auto)
 
 # (a)
-sapply(auto, class)
+sapply(Auto, class)
 # quantitative: mpg, cylinders, displacement, horsepower, weight,
 # acceleration, year
 # qualitative: name, origin
